b78e3487c3fc526fafd28e745a04502ba7436515
Kate <mhq0612@163.com>
2018-10-11 14:52:49 +0100

    closure functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 // Create a function which stores a number. Return an inner function which accepts a number as an argument, adds it to the number in parent scope and returns the new total
 
-function increase(){
-
+function increase() {
   let number = 3;
 
-  function inner(newNumber){
+  function inner(newNumber) {
     number = newNumber + number;
     return number;
   }
@@ -12,4 +11,124 @@ function increase(){
   return inner;
 }
 
-exports.increase = increase;
+function double() {
+  let number = 1;
+
+  function inner() {
+    number = number * 2;
+    return number;
+  }
+
+  return inner;
+}
+
+function total(start) {
+  let total = start;
+  function inner(arr) {
+    total = total + arr.reduce((acc, item) => acc + item);
+    return total;
+  }
+  return inner;
+}
+
+function gibberish() {
+  let string = "hello";
+  function inner(newStrings) {
+    if (typeof newStrings == "string") {
+      string = string.concat(" ", newStrings);
+      return string;
+    } else {
+      const newJoinedString = newStrings.join(" ");
+      string = string.concat(" ", newJoinedString, ".");
+      return string;
+    }
+  }
+
+  return inner;
+}
+
+function mergeSort() {
+  let arr = [];
+  function inner(number) {
+    arr = arr.concat(number);
+
+    sortedArr = arr.sort((a, b) => b - a);
+    return sortedArr[0];
+  }
+  return inner;
+}
+
+function calculator() {
+  let total = 1;
+  function inner(a, b, operator) {
+    if (operator == "+") {
+      return (total = total + (a + b));
+    } else if (operator == "-") {
+      return (total += a - b);
+    } else if (operator == "*") {
+      return (total += a * b);
+    } else if (operator == "/") {
+      return (total += a / b);
+    }
+  }
+  return inner;
+}
+
+function dogHome(){
+  let data={"london":[],"florence":[]}
+  const dogs={
+    houseDog: function(dogObj){
+      Object.keys(data).forEach(loc => {
+        if(dogObj.location == loc) {
+          data[loc]=data[loc].concat(dogObj);
+
+        } })
+        return data
+
+    },
+    getDogsByLocation: function(location) {
+      if( Object.keys(data).find((element)=>element==location) ) {
+        return data[location];
+      }
+    }
+  }
+  return dogs;
+}
+
+function trainstation(){
+  let peopleArr=[{name:"Kate",amount:14},{name:"Tom",amount:17},{name:"Dan",amount:23}];
+  const objMethod={
+    arrive: function(newPersonObj){
+      return peopleArr = peopleArr.concat(newPersonObj)
+    },
+    getPeople: function(){
+      return peopleArr;
+    },
+    giveMoney: function(randomMoney,randomPersonIndex){
+
+      peopleArr[randomPersonIndex].amount+=randomMoney;
+      return peopleArr;
+    },
+    trainArrives: function(){
+      peopleLeft = peopleArr.filter(person => person.amount>=20);
+      peopleArr = peopleArr.filter(person => person.amount < 20);
+      return {peopleArr:peopleArr, peopleLeft:peopleLeft}
+    }
+  }
+  return objMethod;
+}
+
+function shop(){
+  let storage={};
+  let revenue;
+
+  const objMethod={
+    addStock: function(){
+
+    }
+  }
+  return objMethod;
+
+}
+
+module.exports = {trainstation, dogHome, calculator, increase, double, total, gibberish, mergeSort };
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,11 +1,98 @@
-const { increase } = require('../src/index.js');
+const {
+  trainstation,
+  dogHome,
+  calculator,
+  mergeSort,
+  gibberish,
+  total,
+  increase,
+  double
+} = require("../src/index.js");
 
-test('increase', function(){
+test("increase", function() {
   const arg = 10;
   const inner = increase();
   const result = inner(arg);
 
-  const expected = 12;
+  const expected = 13;
 
   expect(result).toBe(expected);
 });
+
+test("double", function() {
+  const inner = double();
+  const result = inner();
+
+  const expected = 2;
+  expect(result).toEqual(expected);
+});
+
+test("total", function() {
+  const arr = [1, 2, 3];
+  const inner = total(2);
+  const result = inner(arr);
+  const expected = 8;
+  expect(result).toEqual(expected);
+});
+
+test("gibberish - array", function() {
+  const newStrings = ["today", "Wed", "Sunny"];
+  const inner = gibberish();
+  const result = inner(newStrings);
+  expect(result).toEqual("hello today Wed Sunny.");
+});
+
+test("gibberish - string", function() {
+  const newStrings = "how is it going";
+  const inner = gibberish();
+  const result = inner(newStrings);
+  expect(result).toEqual("hello how is it going");
+});
+
+test("mergeSort", function() {
+  const inner = mergeSort();
+  inner(3);
+  inner(4);
+  inner(9);
+  const result = inner(6);
+  expect(result).toEqual(9);
+});
+
+test("calculator", function() {
+  const inner = calculator();
+  const result = inner(2, 3, "*");
+  expect(result).toEqual(7);
+});
+
+test("dogHome", function(){
+  const dogs = dogHome();
+  const houseDog=dogs.houseDog({name:"lovely",breed:"husky",location:"london"});
+  const getDogsByLocation=dogs.getDogsByLocation("london");
+  expect(houseDog).toEqual({london:[{name:"lovely",breed:"husky",location:"london"}],florence:[]})
+  expect(getDogsByLocation).toEqual([{name:"lovely",breed:"husky",location:"london"}])
+})
+
+test("trainstation - arrive and getPeople", function(){
+  const objMethod = trainstation();
+  const arrive = objMethod.arrive({name:"Jim",amount:20});
+  expect(arrive).toEqual([{name:"Kate",amount:14},{name:"Tom",amount:17},{name:"Dan",amount:23},{name:"Jim",amount:20}])
+  const getPeople = objMethod.getPeople();
+  expect(getPeople).toEqual([{name:"Kate",amount:14},{name:"Tom",amount:17},{name:"Dan",amount:23},{name:"Jim",amount:20}])
+})
+
+test("trainstation - giveMoney", function(){
+  const objMethod = trainstation();
+  const getPeople = objMethod.getPeople();
+  const randomPersonIndex = Math.floor(Math.random()*(getPeople.length));
+  const randomMoney = Math.floor(Math.random()*20+1);
+  const money = getPeople[randomPersonIndex].amount+randomMoney;
+  const giveMoney = objMethod.giveMoney(randomMoney,randomPersonIndex);
+  expect(giveMoney[randomPersonIndex].amount).toEqual(money)
+
+})
+
+test("trainstation-trainArrives", function(){
+  const objMethod = trainstation();
+  const trainArrives = objMethod.trainArrives();
+  expect(trainArrives).toEqual({peopleArr:[{name:"Kate",amount:14},{name:"Tom",amount:17}],peopleLeft:[{name:"Dan",amount:23}]})
+})
